3a873d4c67ac4e0de13b7f598c9798a5eaf6abca
Obua Denis Daniel <dd.obua@gmail.com>
2024-03-05 17:30:34 +0300

    Implement add book functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const booksElem = document.getElementById('book-collection');
+const btnAddBook = document.getElementById('btn-add-book');
+const inputTitle = document.getElementById('input-title');
+const inputAuthor = document.getElementById('input-author');
 
-const bookCollection = [{ title: 'Power', author: 'Denis' }];
-
-const addNewBook = (title, author) => bookCollection.push({ author, title });
+const bookCollection = [];
 
 const removeBook = (id) => bookCollection.filter((book) => book.id !== id);
 
 const renderBook = () => {
+  booksElem.innerHTML = '';
+
   return bookCollection.forEach((book) => {
     const bookMarkup = `
       <li>
@@ -17,9 +20,25 @@ const renderBook = () => {
         <button>Remove</button>
       </li>
     `;
-
     booksElem.insertAdjacentHTML('beforeend', bookMarkup);
   });
 };
 
-renderBook();
+btnAddBook.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const title = inputTitle.value;
+  const author = inputAuthor.value;
+
+  if (!title) return;
+  if (!author) return;
+
+  bookCollection.push({ title, author });
+
+  inputTitle.value = '';
+  inputAuthor.value = '';
+
+  e.preventDefault();
+
+  renderBook();
+});
