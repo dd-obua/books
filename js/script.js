0197cc3fb5f4a9273986a9a9f6df186202449b0a
Obua Denis Daniel <dd.obua@gmail.com>
2024-03-05 16:51:50 +0300

    Create a function to remove a book from the collection

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@
 const bookCollection = [];
 
 const addNewBook = (author, title) => bookCollection.push({ author, title });
+
+const removeBook = (id) => bookCollection.filter((book) => book.id !== id);
