32f34d8a4fbd2261e26205687865cee83f5c5bfc
Obua Denis Daniel <dd.obua@gmail.com>
2024-03-05 17:02:14 +0300

    Add hard coded value of bookCollection array to bookElem ul

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const bookCollection = [];
+const bookCollection = [{ title: 'Power', author: 'Denis' }];
 
-const addNewBook = (author, title) => bookCollection.push({ author, title });
+const addNewBook = (title, author) => bookCollection.push({ author, title });
 
 const removeBook = (id) => bookCollection.filter((book) => book.id !== id);
+
+const booksElem = document.getElementById('book-collection');
+
+bookCollection.forEach((book) => {
+  const bookMarkup = `
+    <li>
+      <span class="title">${book.title}</span>
+      <span class="author">${book.author}</span>
+      <button>Remove</button>
+    </li>
+  `;
+
+  booksElem.insertAdjacentHTML('beforeend', bookMarkup);
+});
